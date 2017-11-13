86a464c97df56c1cf092b7a9e908c7747b2c1ec0
zadspecial <zadspecial@gmail.com>
2017-11-13 18:38:54 +0600

    Delete script.js

--- .gitignore/script.js
+++
@@ -1,7 +0,0 @@
-MathJax.Hub.Config({
-  jax: ["input/TeX","output/SVG", "output/PreviewHTML"],
-  extensions: ["tex2jax.js","MathMenu.js","MathZoom.js", "fast-preview.js", "AssistiveMML.js", "a11y/accessibility-menu.js"],
-  TeX: {
-  extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
-  }
-});
